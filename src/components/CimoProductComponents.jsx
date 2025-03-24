b1bcb88c145aad0070a2f305eab53faa6de223cd
Andrej Simko <4717664+Hipapheralkus@users.noreply.github.com>
2025-03-24 22:07:18 +0100

    email fix

--- src/components/CimoProductComponents.jsx
+++ src/components/CimoProductComponents.jsx
@@ -710,7 +710,7 @@ const CimoContact = () => {
               <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
               </svg>
-              <img src="/public/images/emails/email-protected-taosi.webp" alt="E-mail" className="h-6" />
+              <img src="/images/emails/email-protected-taosi.webp" alt="E-mail" className="h-6" />
             </div>
           </div>
         </div>
